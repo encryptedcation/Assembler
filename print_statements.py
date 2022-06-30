460b74147bc09b48fa06f68d0ddf58e7e56f194c
richamuktibodh <richa21086@iiitd.ac.in>
2022-06-30 23:13:21 +0530

    added functions

--- print_statements.py
+++ print_statements.py
@@ -1,6 +1,18 @@
-from params import opcode
-from params import registers
+from params import *
 
+def make_8_bit(num):  # have to check for overflow?
+    num=int(num)
+    con_num=[]
+    while num>=1:
+        rem=num%2
+        con_num.append(str(int(rem)))
+        num=num//2
+    con_num=con_num[::-1]
+    bin = ''.join(con_num)
+    if (len(bin) < 8):
+        bin = '0'*(8-len(bin)) + bin
+    return bin
+    
 # not handling variables
 # assuming input taken as string and split based on space into list
 def printbin(lst):
@@ -26,12 +38,13 @@ def printbin(lst):
         val = opcode[code][0] + '00' + registers[code[1]] + registers[code[2]] + registers[code[3]]
 
     #B,C
-    # elif (code == 'mov'):
-    #     val = opcode[code[0]] + registers[code[1]] 
+ 
     elif (code == 'mov'):
         if (lst[-1][0] == '$'):
-            pass
-            #add stuff
+            needed_num = lst[-1][1:]
+            final_bin = make_8_bit(needed_num)
+            val = opcode[code][0][0] +registers[code[1]] + final_bin
+
         else:
             val = opcode[code][1][0] + '00000' + registers[code[1]] + registers[code[2]]
 
@@ -43,6 +56,28 @@ def printbin(lst):
 
     elif (code == 'cmp'):
         val = opcode[code][0] + '00000' + registers[code[1]] + registers[code[2]]
+
+    elif (code == 'ls'):
+        needed_num = lst[-1][1:]
+        final_bin = make_8_bit(needed_num)
+        val = opcode[code][0][0] +registers[code[1]] + final_bin
+
+    #F
+    elif (code=='hlt'):
+        val = opcode[code][0] + '00000000000'
+
+    #D
+    elif (code == 'ld'):
+        
+        
+
+
+
+# vals = ['hi', '&123']
+# a = int(vals[1][1:])
+# print(a)
+
+
 
 
 
