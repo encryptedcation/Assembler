87cb75e66eb383146b0c2d1a3ba7cdff9ea97b61
richamuktibodh <richa21086@iiitd.ac.in>
2022-07-01 20:18:43 +0530

    added D type

--- print_statements.py
+++ print_statements.py
@@ -1,7 +1,21 @@
 from params import *
 
+instrn_count = 0  # counts number of instructions without variable declarations
+var_lst = []   # stores variables
+# lst has the contents of current instruction
+# assuming input taken as string and split based on space into list
+
+# if (lst[0] != 'var'):
+#     instrn_count += 1
+# else:
+#     var_lst.append(lst[1])
+
+# assuming 1st instruction is at memory address 1
+#############################################
+
+
+
 def make_8_bit(num):  # have to check for overflow?
-    num=int(num)
     con_num=[]
     while num>=1:
         rem=num%2
@@ -14,53 +28,58 @@ def make_8_bit(num):  # have to check for overflow?
     return bin
     
 # not handling variables
-# assuming input taken as string and split based on space into list
+
 def printbin(lst):
     code = lst[0]
     val = ''
     #A
     if (code == 'add'):
-        val = opcode[code][0] + '00' + registers[code[1]] + registers[code[2]] + registers[code[3]]
+        val = opcode[code][0] + '00' + registers[lst[1]] + registers[lst[2]] + registers[lst[3]]
     
     elif (code == 'sub'):
-        val = opcode[code][0] + '00' + registers[code[1]] + registers[code[2]] + registers[code[3]]
+        val = opcode[code][0] + '00' + registers[lst[1]] + registers[lst[2]] + registers[lst[3]]
     
     elif (code == 'mul'):
-        val = opcode[code][0] + '00' + registers[code[1]] + registers[code[2]] + registers[code[3]]
+        val = opcode[code][0] + '00' + registers[lst[1]] + registers[lst[2]] + registers[lst[3]]
 
     elif (code == 'xor'):
-        val = opcode[code][0] + '00' + registers[code[1]] + registers[code[2]] + registers[code[3]]
+        val = opcode[code][0] + '00' + registers[lst[1]] + registers[lst[2]] + registers[lst[3]]
         
     elif (code == 'or'):
-        val = opcode[code][0] + '00' + registers[code[1]] + registers[code[2]] + registers[code[3]]
+        val = opcode[code][0] + '00' + registers[lst[1]] + registers[lst[2]] + registers[lst[3]]
 
     elif (code == 'and'):
-        val = opcode[code][0] + '00' + registers[code[1]] + registers[code[2]] + registers[code[3]]
+        val = opcode[code][0] + '00' + registers[lst[1]] + registers[lst[2]] + registers[lst[3]]
 
     #B,C
  
     elif (code == 'mov'):
         if (lst[-1][0] == '$'):
-            needed_num = lst[-1][1:]
+            needed_num = int(lst[-1][1:])
             final_bin = make_8_bit(needed_num)
-            val = opcode[code][0][0] +registers[code[1]] + final_bin
+            val = opcode[code][0][0] +registers[lst[1]] + final_bin
 
         else:
-            val = opcode[code][1][0] + '00000' + registers[code[1]] + registers[code[2]]
+            val = opcode[code][1][0] + '00000' + registers[lst[1]] + registers[lst[2]]
 
     elif (code == 'div'):
-        val = opcode[code][0] + '00000' + registers[code[1]] + registers[code[2]]
+        val = opcode[code][0] + '00000' + registers[lst[1]] + registers[lst[2]]
 
     elif (code == 'not'):
-        val = opcode[code][0] + '00000' + registers[code[1]] + registers[code[2]]
+        val = opcode[code][0] + '00000' + registers[lst[1]] + registers[lst[2]]
 
     elif (code == 'cmp'):
-        val = opcode[code][0] + '00000' + registers[code[1]] + registers[code[2]]
+        val = opcode[code][0] + '00000' + registers[lst[1]] + registers[lst[2]]
 
     elif (code == 'ls'):
-        needed_num = lst[-1][1:]
+        needed_num = int(lst[-1][1:])
         final_bin = make_8_bit(needed_num)
-        val = opcode[code][0][0] +registers[code[1]] + final_bin
+        val = opcode[code][0][0] +registers[lst[1]] + final_bin
+
+    elif (code == 'rs'):
+        needed_num = int(lst[-1][1:])
+        final_bin = make_8_bit(needed_num)
+        val = opcode[code][0][0] +registers[lst[1]] + final_bin
 
     #F
     elif (code=='hlt'):
@@ -68,7 +87,32 @@ def printbin(lst):
 
     #D
     elif (code == 'ld'):
-        
+        if (lst[-1] in var_lst):
+            for i in range(len(var_lst)):
+                if (var_lst[i] == lst[-1]):
+                    ind = i
+                    break
+            mem_addr = instrn_count + (ind+1)
+            bin_mem_addr = make_8_bit(mem_addr)
+            val = opcode[code][0] + registers[lst[1]] + bin_mem_addr
+        else:
+            pass # handle no variable declared error here?
+
+    elif (code == 'st'):
+        if (lst[-1] in var_lst):
+            for i in range(len(var_lst)):
+                if (var_lst[i] == lst[-1]):
+                    ind = i
+                    break
+            mem_addr = instrn_count + (ind+1)
+            bin_mem_addr = make_8_bit(mem_addr)
+            val = opcode[code][0] + registers[lst[1]] + bin_mem_addr
+        else:
+            pass # handle no variable declared error here?
+
+    # E
+
+
         
 
 
