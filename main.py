0558780351d194a2bcbd0b8107ae4713d4478177
sociallyencrypted <mehularora810@gmail.com>
2022-07-04 05:16:07 +0530

    some basic checks

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from print_statements import *
 5. varNameValidity(varName: str)
 6. regValidity(reg: str)
 7. immediateValidity(imm: str)
-8. lenChecker(line: str)
+8. lenChecker(line: str)lst
 9. isValidMemAddr(line: str)
 10. isLineValid(line: str)
 """
@@ -26,3 +26,42 @@ from print_statements import *
 1. make_8_bit(num)
 2. printbin(lst)
 """
+
+# mehul writing here
+
+"""Checks for list of lines:
+1. Variables declared at the very starting
+2. Redeclaration of variables not allowed
+No use of undefined variables
+No use of undefined labels
+No illegal use of FLAGS register
+
+Only one hlt at end
+No lines after hlt
+
+"""
+
+lineCount = 0  # Counting number of lines entered till now
+lines = []
+variables = []
+labels = {}
+
+while True:
+    try:
+        if lineCount < 256:
+            cmd = input()
+            if isValidCmd(cmd):
+                if isLineValid(cmd):
+                    lines.append(cmd)
+                    lineCount += 1
+                else:
+                    print(f"General Syntax Error on line {lineCount}: {cmd}")
+                    exit()
+            else:
+                print("Error: Invalid Command on line " + str(lineCount) + ": " + cmd)
+                exit()
+        else:
+            print("Error: Too many lines")
+            exit()
+    except EOFError:
+        break
