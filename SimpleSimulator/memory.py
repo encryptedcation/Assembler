e930aab5aa8da121d41213f056bc0ab9f653d96e
Ananya Goyal <78123938+cation03@users.noreply.github.com>
2022-08-05 23:58:03 +0530

    added more functions

--- SimpleSimulator/memory.py
+++ SimpleSimulator/memory.py
@@ -1,4 +1,3 @@
-
 R = {
     "000" : 3,
     "001" : 5,
@@ -92,10 +91,16 @@ def xor(line):
     r2 = line[10:13]
     r3 = line[13:16]
 
-def takeAnd(line):
+def AND(line):
     r1 = line[7:10]
     r2 = line[10:13]
     r3 = line[13:16]
+
+def invert():
+    pass
+
+def compare():
+    pass
 
 def mem(writeAddr):
     pass
@@ -112,6 +117,24 @@ def isValidMemAddr(memAddr):
 def isValidImm(imm):
     pass
 
+def load():
+    pass
+
+def store():
+    pass
+
+def jmp():
+    pass
+
+def jgt():
+    pass
+
+def je():
+    pass
+
+def jlt():
+    pass
+
 r = input() # enter input file name
 
 with open(r , "r") as file:
@@ -126,22 +149,102 @@ with open(r , "r") as file:
             reg1 = line[7:10]
             reg2 = line[10:13]
             reg3 = line[13:]
+
+            if (opcode == "10000"):
+                PC += 1
+                res = add(reg1, reg2, reg3)
+            
+            elif (opcode == "10001"):
+                PC += 1
+                res = sub(reg1, reg2, reg3)
+            
+            elif (opcode == "10110"):
+                PC += 1
+                res = mul(reg1, reg2, reg3)
+
+            elif (opcode == "11010"):
+                PC += 1
+                res = xor(reg1, reg2, reg3)
+
+            elif (opcode == "11011"):
+                PC += 1
+                res = OR(reg1, reg2, reg3)
+            
+            elif (opcode == "11100"):
+                PC += 1
+                res = AND(reg1, reg2, reg3)
+
+            elif (opcode == "10001"):
+                PC += 1
+                res = sub(reg1, reg2, reg3)
+
         
         elif (opcodeType == "B"):
             reg1 = line[5:8]
             imm = line[8:]
+
+            if (opcode == "10010"):
+                PC += 1
+                res = movImm(reg1, imm)
+            
+            elif (opcode == "11001"):
+                PC += 1
+                res = lShift(reg1, imm)
+            
+            elif (opcode == "11000"):
+                PC += 1
+                res = rShift(reg1, imm)
         
         elif (opcodeType == "C"):
             reg1 = line[10:13]
             reg2 = line[13:]
+
+            if (opcode == "10011"):
+                PC += 1
+                res = movReg(reg1, reg2)
+            
+            elif (opcode == "10111"):
+                PC += 1
+                res = divide(reg1, reg2)
+            
+            elif (opcode == "11101"):
+                PC += 1
+                res = invert(reg1, reg2)
+            
+            elif (opcode == "11110"):
+                PC += 1
+                res = compare(reg1, reg2)
         
         elif (opcodeType == "D"):
             reg1 = line[5:8]
             memAddr = line[8:]
+
+            if (opcode == "10100"):
+                PC += 1
+                res = load(reg1, memAddr)
+            
+            elif (opcode == "10101"):
+                PC += 1
+                res = store(reg1, memAddr)
         
         elif (opcodeType == "E"):
-            reg1 = line[10:13]
-            reg2 = line[13:]
+            memAddr = line[8:]
+
+            if (opcode == "11111"):
+                PC = 'idk'
+                res = jmp(memAddr)
+            
+            elif (opcode == "01100"):
+                PC = 'idk'
+                res = jlt(memAddr)
+            
+            elif (opcode == "01101"):
+                PC = 'idk'
+                res = jgt(memAddr)
+            
+            elif (opcode == "01111"):
+                PC = 'idk'
+                res = je(memAddr)
         
         elif (opcodeType == "F"):
             hltFlag = 1
